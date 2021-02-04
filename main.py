d6cb628e071c15323134d949c13e5a2ea69d3a63
Shirayan <nenenel@yandex.ru>
2021-02-04 22:24:35 +0300

    xd1111

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from ursina import *
-
+class Test_cube(Entity):
+    def __init__(self):
+        super().__init__(
+            model ='cube',
+            color = color.white,
+            texture = 'white_cube'
+            rotation = Vec3(0,0,0)
+            )
+def update():
+    if held_keys['a']:
+        test_square.x -= 3 * time.dt
+    if held_keys['d']:
+        test_square.x += 3 * time.dt
 app = Ursina()
+test_square = Entity (model = 'quad', color = color.red, scale = (1,4), position = (5,4 ))
+sans = Entity (model = 'quad', texture = 'sans.png')
+
+test_cube = Test_cube()
+
 app.run()
